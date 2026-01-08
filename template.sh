57ba93aaa7871b399c834058752a2233d88cdd33
spha-code <hadjidjanos@gmail.com>
2026-01-08 16:48:56 +0100

    Folder Structure

---
+++ template.sh
@@ -0,0 +1,15 @@
+#Create the Directories of the Medical-Bot
+mkdir -p src
+mkdir -p research
+
+#Create the Files within the Directories
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+
+echo "Directories and Files Created Succesfully!"
